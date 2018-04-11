48a3d9b94bdea6caf46074bbb270c13859972897
Ray Tham <RayWTham@gmail.com>
2018-04-11 15:31:22 +0800

    added comments

--- play.rb
+++ play.rb
@@ -10,7 +10,7 @@ require './game'
 require './cards'
 
 playing = true
-
+# creates a new instance of a game from  Game class (from game.rb)
 game = Game.new
 
 puts "What is your name?"
@@ -18,20 +18,24 @@ user_name = gets.chomp
 
 while playing
 
+  # calls play method from game
   game.play
+  
+  # creates a new entry in hands TABLE
   Hand.create(user_hand: "#{game.hands[0]}", house_hand: "#{game.hands[1]}", user_name: "#{user_name}")
-
   puts "current score is: #{game.check_game.to_s}"
 
+  #stop or contiune game
   puts "do you want to play this hand?"
-
   answer = gets.chomp
   if answer == "no"
     puts "folding"
     next
   end
 
+  #end game condition
   if game.check_game < -2
     playing = false
   end
+
 end
