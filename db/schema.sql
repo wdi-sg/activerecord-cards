4ab848004c99cb86263b30a3e447a5145ff2ea76
jerrypan90 <jerrypan_90@hotmail.com>
2018-04-12 21:09:15 +0800

    added completed homework including further

--- db/schema.sql
+++ db/schema.sql
@@ -1,4 +1,16 @@
 DROP TABLE IF EXISTS hands;
 
-CREATE TABLE hands(
+CREATE TABLE hands
+(
+    id SERIAL PRIMARY KEY,
+    card TEXT,
+    users_id INT
+);
+
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users
+(
+    id SERIAL PRIMARY KEY,
+    name TEXT
 );
