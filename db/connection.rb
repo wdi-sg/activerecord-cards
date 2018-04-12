da4138ccaa849389ad0b1dc8eda3d757200dbf71
Edmund <edmundcjj@gmail.com>
2018-04-13 00:59:35 +0800

    Completed all including further

--- db/connection.rb
+++ db/connection.rb
@@ -1,4 +1,8 @@
 ActiveRecord::Base.establish_connection(
   :adapter => "postgresql",
-  :database => "cardgame"
+  :database => "cardgame",
+  :user => 'postgres',
+  :host => '127.0.0.1',
+  :port => 5432,
+  :password => 'edmund92chow'
 )
--- game.rb
+++ game.rb
@@ -1,16 +1,21 @@
 class Game
+  attr_accessor :hands, :current_hand
 
   def initialize
     @deck = Cards.new
     @score = 0
+    @current_hand = []
     @hands = []
   end
 
   def play
+    @current_hand = []
     user_hand = @deck.show_card
     house_hand = @deck.show_card
 
-    @hands << [user_hand, house_hand]
+    @current_hand.push(user_hand)
+    @current_hand.push(house_hand)
+    @hands.push(@current_hand)
 
     if user_hand > house_hand
       @score += 1
